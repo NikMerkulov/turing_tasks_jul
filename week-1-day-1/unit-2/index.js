dc41a411c36ef7eca832e525131fe7168b0a3daf
NikMerkulov <RikoStation@gmail.com>
2021-07-25 02:22:43 +0700

    Adding unit 2 from the first day and making minor orginization changes.

---
+++ week-1-day-1/unit-2/index.js
@@ -0,0 +1,35 @@
+// Сниппет адаптивного мэпинга картинки.
+window.onload = function () {
+  var ImageMap = function (map, img) {
+      var n,
+        areas = map.getElementsByTagName("area"),
+        len = areas.length,
+        coords = [],
+        previousWidth = 1000 // Ширина загружаемой картинки
+      for (n = 0; n < len; n++) {
+        coords[n] = areas[n].coords.split(",")
+      }
+      this.resize = function () {
+        var n,
+          m,
+          clen,
+          x = img.offsetWidth / previousWidth
+        for (n = 0; n < len; n++) {
+          clen = coords[n].length
+          for (m = 0; m < clen; m++) {
+            coords[n][m] *= x
+          }
+          areas[n].coords = coords[n].join(",")
+        }
+        previousWidth = document.body.clientWidth
+        return true
+      }
+      window.onresize = this.resize
+    },
+    imageMap = new ImageMap(
+      document.getElementById("browsers-map"),
+      document.getElementById("browsers")
+    )
+  imageMap.resize()
+  return
+}
